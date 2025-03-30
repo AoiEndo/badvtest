c04fafd7186040a686b63117dc8c0e5a7f3d7350
Aoi Endo <24rb1033@student.gs.chiba-u.jp>
2025-03-30 17:41:14 +0900

    testpage add

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+// filepath: /Users/aoiendo/MyProjects/BadV/badvtest/app/about/page.tsx
+export default function About() {
+    return (
+      <div className="p-8">
+        <h1 className="text-2xl font-bold">About Page</h1>
+        <p className="mt-4 text-lg">
+          This is the About page of the application.
+        </p>
+      </div>
+    );
+  }
